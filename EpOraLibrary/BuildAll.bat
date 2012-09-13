5fb984b0343b2e263808212cf3c1d239ff2c4f08
Woong Gyu La <juhgiyo@gmail.com>
2012-09-14 08:20:50 +0900

    BuildAll Batch updated

--- EpOraLibrary/BuildAll.bat
+++ EpOraLibrary/BuildAll.bat
@@ -1,3 +1,5 @@
+if not exist "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv.exe" goto :VS9Compile
+
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Debug" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Debug"
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Debug DLL" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Debug DLL"
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Debug DLL Unicode" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Debug DLL Unicode"
@@ -6,6 +8,10 @@
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Release DLL" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Release DLL"
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Release DLL Unicode" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Release DLL Unicode"
 "C:\Program Files\Microsoft Visual Studio 8\Common7\IDE\devenv" EpOraLibrary80.sln /build "Release Unicode" /project "EpOraLibrary\EpOraLibrary80.vcproj" /projectconfig "Release Unicode"
+
+:VS9Compile
+
+if not exist "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" goto :VS10Compile
 
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" EpOraLibrary90.sln /build "Debug" /project "EpOraLibrary\EpOraLibrary90.vcproj" /projectconfig "Debug"
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" EpOraLibrary90.sln /build "Debug DLL" /project "EpOraLibrary\EpOraLibrary90.vcproj" /projectconfig "Debug DLL"
@@ -16,6 +22,10 @@
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" EpOraLibrary90.sln /build "Release DLL Unicode" /project "EpOraLibrary\EpOraLibrary90.vcproj" /projectconfig "Release DLL Unicode"
 "C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv" EpOraLibrary90.sln /build "Release Unicode" /project "EpOraLibrary\EpOraLibrary90.vcproj" /projectconfig "Release Unicode"
 
+:VS10Compile
+
+if not exist "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" goto :Done
+
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Debug" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Debug"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Debug DLL" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Debug DLL"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Debug DLL Unicode" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Debug DLL Unicode"
@@ -24,3 +34,5 @@
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Release DLL" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Release DLL"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Release DLL Unicode" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Release DLL Unicode"
 "C:\Program Files\Microsoft Visual Studio 10.0\Common7\IDE\devenv" EpOraLibrary100.sln /build "Release Unicode" /project "EpOraLibrary\EpOraLibrary100.vcxproj" /projectconfig "Release Unicode"
+
+:Done
